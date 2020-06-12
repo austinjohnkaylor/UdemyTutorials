5341397a2fd3d3a4bf7a65b90c00edd6850de2fa
Austin Kaylor <akaylor@singlesourceproperty.com>
2020-06-12 16:36:36 -0400

    Adding a project for the single responsability principle

---
+++ DesignPatternsInCsharpAndDotnet/SOLID.SingleResponsabilityPrinciple/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SOLID.SingleResponsabilityPrinciple
+{
+    class Program
+    {
+        public class Journal
+        {
+            private readonly List<string> entries = new List<string>();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
